f7f019de530dd84751face0afc7a17044cfd2c92
Alban Munari <alban.munari@phoebe.fr>
2026-09-07 11:21:31 +0200

    feat: adding global variable & clipboard conf & basic userCmd

--- modules/editor/nixvim.nix
+++ modules/editor/nixvim.nix
@@ -5,10 +5,24 @@
     ./opts.nix
     ./autoCmd.nix
     ./plugins
+    ./userCmd.nix
   ];
 
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
+
+    globals = {
+      mapleader = " ";
+      maplocalleader = " ";
+      loaded_ruby_provider = 0;
+      loaded_perl_provider = 0;
+    };
+
+    clipboard = {
+      register = "unnamedplus";
+      providers.wl-copy.enable = true;
+    };
+
   };
 }
---
+++ modules/editor/userCmd.nix
@@ -0,0 +1,12 @@
+programs.nixvim.userCommands = {
+  # Force Save
+  W = {
+    command = "w !sudo tee % > /dev/null";
+    desc = "Save file with sudo";
+  };
+  # Trim Spaces
+  TrimWhitespace = {
+    command = "%s/\\s\\+$//e";
+    desc = "Remove trailing whitespace";
+  };
+};
