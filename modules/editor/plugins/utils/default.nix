c322e09e1ec7b2f629f650b2cae7a44076519db2
Alban Munari <alban.munari@phoebe.fr>
2026-09-07 15:03:30 +0200

    feat: adding which-key plugin

--- modules/editor/plugins/utils/default.nix
+++ modules/editor/plugins/utils/default.nix
@@ -2,5 +2,6 @@
     imports = [
         ./gitsigns.nix
         ./fugitive.nix
+        ./whichkey.nix
     ];
 }
---
+++ modules/editor/plugins/utils/whichkey.nix
@@ -0,0 +1,39 @@
+{
+  programs.nixvim.plugins.which-key = {
+    enable = true;
+
+    settings = {
+      delay = 300;
+      preset = "modern";
+
+      win = {
+        border = "rounded";
+        padding = [ 1 2 ];
+        title = true;
+      };
+
+      spec = [
+        {
+          __unkeyed-1 = "<leader>b";
+          group = "Buffers";
+        }
+        {
+          __unkeyed-1 = "<leader>c";
+          group = "Code / LSP";
+        }
+        {
+          __unkeyed-1 = "<leader>f";
+          group = "Find (Telescope)";
+        }
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "Git (Fugitive)";
+        }
+        {
+          __unkeyed-1 = "<leader>h";
+          group = "Git Hunks (Gitsigns)";
+        }
+      ];
+    };
+  };
+}
