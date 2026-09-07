866c3dcdb85d11a6f067b386f744a533d3debe48
Alban Munari <alban.munari@phoebe.fr>
2026-09-07 12:09:39 +0200

    feat: adding lualine plugin

---
+++ modules/editor/plugins/lualine.nix
@@ -0,0 +1,47 @@
+{
+    programs.nixvim.plugins.lualine = {
+        enable = true;
+
+        settings = {
+            options = {
+                theme = "rose-pine";
+                globalstatus = true;
+                icons_enabled = false;
+
+                component_separators = { 
+                    left = "|"; 
+                    right = "|"; 
+                };
+                section_separators = {
+                    left = "|";
+                    right = "|";
+                };
+            };
+
+            sections = {
+                lualine_a = [ "mode" ];
+                lualine_b = [ "branch" "diff" ];
+                lualine_c = [
+                    {
+                        __unkeyed-1 = "filename";
+                        path = 1;
+                        symbols = {
+                            modified = " [+] ";
+                            readonly = " [RO] ";
+                            unnamed = " [No Name] ";
+                        };
+                    }
+                ];
+                lualine_x = [
+                    {
+                        __unkeyed-1 = "diagnostics";
+                        sources = [ "nvim_lsp" ];
+                    }
+                    "filetype"
+                ];
+                lualine_y = [ "progress" ];
+                lualine_z = [ "location" ];
+            };
+        };
+    };
+}
